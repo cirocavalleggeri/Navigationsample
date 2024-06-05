773eeaaeaebf8c8b07d6f8fddaa76049e14ed492
cirocavalleggeri <ciroallinone@gmail.com>
2024-06-05 14:37:31 +0200

    Initial commit
     In questa app di Denis Panjuta si passa da uno schermo ad un altro schermo usando il NavController.
     Il NavController deve essere importato in gradle dell'app ed eseguire nell' activity principale uno specifico codice nella funzione MyApp()

---
+++ app/src/main/java/com/example/navigationsample/SecondScreen.kt
@@ -0,0 +1,44 @@
+package com.example.navigationsample
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun SecondScreen(navigationToFirstScreen:()->Unit){
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(text = "Seconda schermata", fontSize = 16.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "WELCOME!", fontSize = 25.sp)
+        Button(onClick = { navigationToFirstScreen()}) {
+            Text(text = "Vai a Prima  schermata", fontSize = 16.sp)
+        }
+    }
+}
+@Preview(showBackground=true)
+@Composable
+fun SecondScreenPreview(){
+    SecondScreen({})
+}
